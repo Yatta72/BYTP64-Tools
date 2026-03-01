9d2e6449c0a919caae730fc37d32a77f6b739897
BYTP64 <zaksalah2014@gmail.com>
2026-03-01 07:38:38 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -179,7 +179,7 @@ async def on_ready():
     # Set the bot's status
     await bot.change_presence(
         activity=Game(name="Dandy's World"),
-        status=discord.Status.dnd  # Can be online, idle, dnd (do not disturb), or invisible
+        status=discord.Status.online  # Can be online, idle, dnd (do not disturb), or invisible
     )
 
 @bot.command(name="video")
